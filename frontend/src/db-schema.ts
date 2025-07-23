9ff2bd22f29e75f9587e0e082d7a56cf8bc7ad7c
BigZoo92 <givernaudenzo@gmail.com>
2025-07-24 00:36:00 +0200

    dx: init local dev

---
+++ frontend/src/db-schema.ts
@@ -0,0 +1,27 @@
+export type UserRole = 'standard' | 'admin';
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  role: UserRole;
+  created_at: string;
+};
+
+export type Product = {
+  id: number;
+  user_id: number;
+  title: string;
+  description: string | null;
+  price: number | null;
+  created_at: string;
+};
+
+export type Comment = {
+  id: number;
+  user_id: number;
+  product_id: number;
+  content: string;
+  created_at: string;
+};
