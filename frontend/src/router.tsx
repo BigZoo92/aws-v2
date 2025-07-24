923bc4030b950e1642eb8bd2d5a670c4f019c083
BigZoo92 <givernaudenzo@gmail.com>
2025-07-24 13:46:37 +0200

    resolve conflicts

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,14 +1,25 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Auth from './pages/auth/auth';
+import ProductCRUD from './pages/products/ProductCRUD';
+import Homepage from './pages/Homepage';
+import ProductPage from './pages/products/ProductPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Auth />,
+    element: <Homepage />,
   },
   {
     path: '/signup',
     element: <Auth />,
+  },
+  {
+    path: '/products',
+    element: <ProductCRUD />,
+  },
+  {
+    path: '/products/:id',
+    element: <ProductPage />,
   },
 ]);
 
